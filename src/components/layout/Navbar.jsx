56789a450023f63969d9270343957e9bc78dc407
Larissa <larissa.fraga@ufms.br>
2022-03-10 08:50:11 -0400

    Implemantation of project update

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,25 +7,25 @@ import logo from "../../img/costs_logo.png";
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <Container>
-        <Link to="/">
-          <img src={logo} alt="costs logo" />
-        </Link>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to="/company">Company</Link>
-          </li>
-        </ul>
-      </Container>
+        <Container className={styles.div_container}>
+          <Link to="/">
+            <img src={logo} alt="costs logo" />
+          </Link>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={styles.item}>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className={styles.item}>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className={styles.item}>
+              <Link to="/company">Company</Link>
+            </li>
+          </ul>
+        </Container>
     </div>
   );
 }
--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,6 +1,8 @@
 import styles from "./Project.module.css";
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
+import ProjectForm from "../project/ProjectForm";
+import Message from "../layout/Message";
 
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -10,6 +12,8 @@ function Project() {
 
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
 
   useEffect(() => {
     setTimeout(() => {
@@ -27,6 +31,34 @@ function Project() {
     }, 300);
   }, [id]);
 
+  function editPost(project) {
+    // budget validation
+
+    if(project.budget < project.cost) {
+      setMessage("Budget cannot be less than cost");
+      setType("error");
+      return false;
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setProject(data);
+      setShowProjectForm(false);
+      setMessage("Project updated successfully");
+      setType("success");
+    })
+    
+    .catch((err) => console.log(err));
+
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
@@ -36,6 +68,7 @@ function Project() {
       {project.name ? (
         <div className={styles.project_details}> 
           <Container customClass="column">
+            {message && <Message type={type} msg={message}/>}
             <div className={styles.details_container}>
               <h1>Project: {project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
@@ -55,7 +88,7 @@ function Project() {
                 </div>
               ) : (
                 <div className={styles.project_info}>
-                  <p>form</p>
+                  <ProjectForm handleSubmit={editPost} btnText="Finish edit" projectData={project}/>
                 </div>
               )}
             </div>
